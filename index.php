30402dddce1dcdfe80cd91410a1959784b2de413
Harald Leithner <leithner@itronic.at>
2019-06-29 20:01:25 +0200

    Dump test.

--- index.php
+++ index.php
@@ -32,5 +32,7 @@ if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
  */
 define('_JEXEC', 1);
 
+var_dump($_ENV);
+
 // Run the application - All executable code should be triggered through this file
 require_once dirname(__FILE__) . '/includes/app.php';
