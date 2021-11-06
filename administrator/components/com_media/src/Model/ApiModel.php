89989a3aa022470680ab01595acb6a00bb658c64
Harald Leithner <leithner@itronic.at>
2021-11-07 00:01:28 +0100

    better catch exception getFileEvent test4 drone debugging 22

--- administrator/components/com_media/src/Model/ApiModel.php
+++ administrator/components/com_media/src/Model/ApiModel.php
@@ -119,8 +119,8 @@ class ApiModel extends BaseDatabaseModel
 		$file->path    = $adapter . ":" . $file->path;
 		$file->adapter = $adapter;
 
-		$event = new FetchMediaFileEvent('onFetchMediaFile', ['file' => $file]);
 			return $file;
+		$event = new FetchMediaFileEvent('onFetchMediaFile', ['file' => $file]);
 		Factory::getApplication()->getDispatcher()->dispatch($event->getName(), $event);
 
 		return $event->getArgument('file');
